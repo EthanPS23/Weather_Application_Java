7ac30d1f777ec1e1112166596349495437ae12e6
Ethan S <ethanps23@gmail.com>
2020-02-03 10:18:20 -0700

    created method to insert single station info

--- src/sample/Model/StationInfoDB.java
+++ src/sample/Model/StationInfoDB.java
@@ -49,4 +49,22 @@ public class StationInfoDB {
 
         return result;
     }
+
+    public static boolean SetStationInfo(StationInfo stationInfo){
+        var result = false;
+        // sql to insert into the StationInfo table. If a duplicate key exists then the values will be updated
+        var sql = "INSERT INTO StationInfo " +
+                "(StationID,RegionID,StationName,Elevation,Latitude,Longitude,Description) " +
+                "VALUES " +
+                "('%1$s','%2$s','%3$s','%4$s','%5$s','%6$s','%7$s')" +
+                "ON DUPLICATE KEY UPDATE StationName='%3$s',Elevation='%4$s',Latitude='%5$s',Longitude='%6$s'," +
+                "Description='%7$s';";
+
+        var sqlStatement = String.format(sql, stationInfo.getStationID(), stationInfo.getRegionID(),
+                stationInfo.getStationName(), stationInfo.getElevation(), stationInfo.getLatitude(),
+                stationInfo.getLongitude(), stationInfo.getDescription());
+        result = SQL.ExecuteUpdate(sqlStatement)>=0; // TODO:if the number of rows affected is 0 or more than result is true
+
+        return result;
+    }
 }
